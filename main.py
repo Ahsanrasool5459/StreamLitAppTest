e76d3d7f9d655fab78a6d63a717d5ad88aa31021
Ahsanrasool5459 <149204486+Ahsanrasool5459@users.noreply.github.com>
2023-10-28 02:41:00 +0500

    Update main.py
    Testing with Emogis

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import streamlit
 streamlit.title('Dinner Menu')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-
-
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Ranged Egg')
+streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
