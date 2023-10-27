b193f071e1606c25f7caab7f814a99b4f01f44cd
Ahsanrasool5459 <149204486+Ahsanrasool5459@users.noreply.github.com>
2023-10-28 02:50:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -18,4 +18,4 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 
-streamlit.dataframe(my_fruit_list.index)
+streamlit.dataframe(my_fruit_list)
