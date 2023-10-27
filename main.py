ee13080fdd7aad2ba53681d66fe041c3a1cfa7f8
Ahsanrasool5459 <149204486+Ahsanrasool5459@users.noreply.github.com>
2023-10-28 02:45:35 +0500

    Update main.py
    using pandas with streamline

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import streamlit
+import pandas as pd
+df=pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+
 streamlit.title('Dinner Menu')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -8,3 +11,4 @@ streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+streamlit.dataframe(df)
